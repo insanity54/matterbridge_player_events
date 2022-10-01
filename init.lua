229cbe7edaa9bee5526107f22336701bf8ec8659
Chris Grimmett <chris@grimtech.net>
2022-09-30 23:01:06 -0700

    fix cheat reporting

--- init.lua
+++ init.lua
@@ -28,20 +28,21 @@ minetest.register_on_leaveplayer(
 
 minetest.register_on_cheat(
     function (player, cheat)
-        for key, val in pairs(cheat) do  -- Table iteration.
-          print(key, val)
-        end
-        if cheat == 'moved_too_fast' then
+        -- for key, val in pairs(cheat) do  -- Table iteration.
+        --   print(key, val)
+        -- end
+        print(cheat.type)
+        if cheat.type == 'moved_too_fast' then
             yl_matterbridge:send_to_bridge(server_chat_name, player:get_player_name() .. ' cheated. (moved too fast)')
-        elseif cheat == 'interacted_too_far' then
+        elseif cheat.type == 'interacted_too_far' then
             yl_matterbridge:send_to_bridge(server_chat_name, player:get_player_name() .. ' cheated. (interacted too far)')
-        elseif cheat == 'interacted_while_dead' then
+        elseif cheat.type == 'interacted_while_dead' then
             yl_matterbridge:send_to_bridge(server_chat_name, player:get_player_name() .. ' cheated. (interacted while dead)')
-        elseif cheat == 'finished_unknown_dig' then
+        elseif cheat.type == 'finished_unknown_dig' then
             yl_matterbridge:send_to_bridge(server_chat_name, player:get_player_name() .. ' cheated. (finished unknown dig)')
-        elseif cheat == 'dug_unbreakable' then
+        elseif cheat.type == 'dug_unbreakable' then
             yl_matterbridge:send_to_bridge(server_chat_name, player:get_player_name() .. ' cheated. (dug unbreakable)')
-        elseif cheat == 'dug_too_fast' then
+        elseif cheat.type == 'dug_too_fast' then
             yl_matterbridge:send_to_bridge(server_chat_name, player:get_player_name() .. ' cheated. (dug too fast)')
         end
     end
