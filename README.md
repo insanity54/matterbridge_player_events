4dcdc64bcc548dc19a43e006c20ebb5bd61f5af4
Chris Grimmett <chris@grimtech.net>
2022-10-01 17:06:02 -0700

    make features configurable

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # yl_matterbridge_player_events Minetest mod
 
+Requires the [yl_matterbridge](https://content.minetest.net/packages/AliasAlreadyTaken/yl_matterbridge/) mod which must be configured first for this mod to work.
+
 This mod adds the following features
 
   * When a player joins, a message is sent to yl_matterbridge
@@ -7,5 +9,10 @@ This mod adds the following features
   * When a player leaves, a message is sent to yl_matterbridge
   * When a player cheats, a message is sent to yl_matterbridge
 
+Each feature is optional, and can be enabled/disabled with the following settings in your minetest.conf
 
-Depends on [yl_matterbridge](https://content.minetest.net/packages/AliasAlreadyTaken/yl_matterbridge/)
+  * yl_matterbridge_player_events.report_join   (default true)
+  * yl_matterbridge_player_events.report_die    (default true)
+  * yl_matterbridge_player_events.report_leave  (default true)
+  * yl_matterbridge_player_events.report_cheats (default false) (Use with caution: this can cause lag because there are lots of false positives which are rapidly fired)
+
--- init.lua
+++ init.lua
@@ -1,34 +1,48 @@
 
 local server_chat_name = 'SERVER'
 
+local function get_setting(setting_name, default)
+    return minetest.settings:get_bool(setting_name) or default
+end
+
 print("[MOD] yl_matterbridge_player_events loaded.")
 
 
-minetest.register_on_joinplayer(
-    function(player)
-        yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' joined.')
-    end
-)
 
-minetest.register_on_dieplayer(
-    function(player)
-        yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' died.')
-    end
-)
 
-minetest.register_on_leaveplayer(
-    function (player, timed_out)
-        if timed_out then
-            yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' timed out.')
-        else
-            yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' left.')
+if get_setting("yl_matterbridge_player_events.report_join", true) then
+    minetest.register_on_joinplayer(
+        function(player)
+            yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' joined.')
         end
-    end
-)
+    )
+end
 
-minetest.register_on_cheat(
-    function (player, cheat)
-        yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' cheated. (' .. cheat.type .. ')')
-    end
-)
+if get_setting("yl_matterbridge_player_events.report_die", true) then
+    minetest.register_on_dieplayer(
+        function(player)
+            yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' died.')
+        end
+    )
+end
+
+if get_setting("yl_matterbridge_player_events.report_leave", true) then
+    minetest.register_on_leaveplayer(
+        function (player, timed_out)
+            if timed_out then
+                yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' timed out.')
+            else
+                yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' left.')
+            end
+        end
+    )
+end
+
+if get_setting("yl_matterbridge_player_events.report_cheats", false) then
+    minetest.register_on_cheat(
+        function (player, cheat)
+            yl_matterbridge.send_to_bridge(server_chat_name, player:get_player_name() .. ' cheated. (' .. cheat.type .. ')')
+        end
+    )
+end
 
